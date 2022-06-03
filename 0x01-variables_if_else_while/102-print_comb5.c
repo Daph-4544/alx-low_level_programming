ed09083c1562b31df18bbdfd8c6536626a0cd8cf
Daph-4544 <daphinebochere@gmail.com>
2022-06-03 16:05:47 -0700

    task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-	int i,j;
+	int i, j;
 
 	for (i = 0; i < 100; i++)
 	{
@@ -14,19 +14,18 @@ int main(void)
 		{
 			if (i < j)
 			{
-				putchar((i / 10) + 48)
-		            	putchar((i % 10) + 48);
-		        	putchar(' ');
-		          	putchar((j / 10) + 48);
-		         	putchar((j % 10) + 48);
-		           	if (i != 98 || j != 99)
+				putchar((i / 10) + 48);
+				putchar((i % 10) + 48);
+				putchar(' ');
+				putchar((j / 10) + 48);
+				putchar((j % 10) + 48);
+				if (i != 98 || j != 99)
 				{
 					putchar(',');
 					putchar(' ');
 				}
 			}
 		}
-
 	}
 	putchar('\n');
 	return (0);
