bb948e4cab884097b35e3eade456bdaf39075163
Daph-4544 <daphinebochere@gmail.com>
2022-06-07 10:48:55 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "stdio.h"
+
+/**
+ * prints alphabets in lowercase letters
+ */
+int main(void)
+
+{
+	char alphabets;
+
+	alphabets = 'a';
+
+	while ( alphabets <= 'z')
+	{
+		putchar(alphabets);
+		alphabets++;
+	}
+	putchar('\n')
+}
+
