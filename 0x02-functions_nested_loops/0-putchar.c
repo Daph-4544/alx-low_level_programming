80dc52e8dfc57b812ca2db4b5c54e3d069457275
Daph-4544 <daphinebochere@gmail.com>
2022-06-07 07:17:51 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+/**
+ * prints _putchar, followed by a newline
+ * Return 0 if successful
+ */
+int main(void)
+{
+	int char;
+	printf ("_putchar");
+	printf ('\n');
+	return 0;
+}
