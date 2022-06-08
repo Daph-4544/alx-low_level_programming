7b9b9e704a4d436da940b3cba83ee7d59b12a1ed
Daph-4544 <daphinebochere@gmail.com>
2022-06-08 00:37:49 -0700

    task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * times_table - it prints 9 times table starting 0
+ * Return: zero
+ */
+void times_table(void)
+{
+	int i, j, multiply;
+
+	for (i = 0; i < 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			multiply = i * j;
+			if (j == 0)
+			{
+				_putchar(multiply + '0');
+			}
+			else
+			{
+				if (multiply < 10)
+				{
+					_putchar(',');
+					_putchar(32);
+					_putchar(32);
+					_putchar(multiply + '0');
+				}
+				else
+				{
+					_putchar(',');
+					_putchar(32);
+					_putchar((multiply / 10) + '0');
+					_putchar((multiply % 10) + '0'
+							}
+							};
+							}
+							_putchar('\n');
+							}
+							}
