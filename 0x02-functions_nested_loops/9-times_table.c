36cfd93a554f9c969357c5cf890a0be4661a9746
Daph-4544 <daphinebochere@gmail.com>
2022-06-08 00:56:33 -0700

    task 10

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -31,7 +31,7 @@ void times_table(void)
 					_putchar(',');
 					_putchar(32);
 					_putchar((multiply / 10) + '0');
-					_putchar((multiply % 10) + '0'
+					_putchar((multiply % 10) + '0');
 							}
 							};
 							}
