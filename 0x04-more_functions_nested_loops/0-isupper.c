47d2e8d06ad56d6e6a7ed89420be4b02f9d2ff36
Daph-4544 <daphinebochere@gmail.com>
2022-06-09 07:40:07 -0700

    task 1

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isupper - function to check uppercase character.
+ * @c: the argument of the _islower function.
+ *
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
