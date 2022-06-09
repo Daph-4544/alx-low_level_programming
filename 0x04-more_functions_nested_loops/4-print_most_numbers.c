88a9a03973063c0b8b378dfc073c92656a03bf06
Daph-4544 <daphinebochere@gmail.com>
2022-06-09 08:27:33 -0700

    task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - function to print digit 0-9
+ * minus 2 & 4
+ *
+ * Return: Always 0.
+ */
+
+void print_most_numbers(void)
+{
+	char n = '0';
+
+	while (n <= '9')
+	{
+		if (n != '2' && n != '4')
+		{
+			_putchar(n);
+		}
+
+		n++;
+	}
+	_putchar('\n');
+}
