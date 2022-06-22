f327f99b46e77a592d93f44875f19be047ce2fda
Daph-4544 <daphinebochere@gmail.com>
2022-06-22 03:40:35 -0700

    printing a string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ *  _puts_recursion - prints a string.
+ * @s: the string of the function
+ * return - always 0
+ */
+void _puts_recursion(char *s)
+{
+	puts("%s\n");
+	return (0);
+}
