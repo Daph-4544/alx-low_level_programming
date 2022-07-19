4ec0375d7a0d325548c58d04c6343d2056d5a014
Daph-4544 <105221640+Daph-4544@users.noreply.github.com>
2022-07-19 10:21:29 +0300

    a function that prints all the elements of a listint_t list.

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,20 +1,21 @@
 #include "lists.h"
 
 /**
- * listint_len - returns the number of elements in a linked lists
- * @h: linked list of type listint_t to traverse
- *
- * Return: number of nodes
+ * print_listint - prints all the elements of list.
+ * @h: head of list.
+ * Return: numbers of nodes.
  */
-size_t listint_len(const listint_t *h)
+
+size_t print_listint(const listint_t *h)
 {
-	size_t num = 0;
+	size_t numnodes;
 
-	while (h)
+	numnodes = 0;
+	while (h != NULL)
 	{
-		num++;
+		printf("%d\n", h->n);
 		h = h->next;
+		numnodes++;
 	}
-
-	return (num);
+	return (numnodes);
 }
